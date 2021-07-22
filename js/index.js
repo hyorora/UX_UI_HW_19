94e7711545fc911ba54ab5b6ebb06917a68dd25d
hyorora <86327951+hyorora@users.noreply.github.com>
2021-07-22 18:49:55 -0400

    everything

---
+++ js/index.js
@@ -0,0 +1,7 @@
+console.log("your index.js file is loaded correctly");
+
+$(".button").click(function() {
+    $(".button").animate({left: "+=250px"},"slow");
+});
+console.log("button animation")
+
