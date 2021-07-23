22d47aaf508c730a21047a1eb76ec7f63d35a207
hyorora <86327951+hyorora@users.noreply.github.com>
2021-07-22 22:14:23 -0400

    chnages

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ console.log("your index.js file is loaded correctly");
 $(".button").click(function() {
     $(".button").animate({left: "+=250px"},"slow");
 });
-console.log("button animation")
+console.log("button animation");
 
